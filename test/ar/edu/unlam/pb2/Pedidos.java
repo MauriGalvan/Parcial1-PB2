13e6b46e8d5bf46973f6902200c1caa37849587d
noelia <noeliaromeroinf@gmail.com>
2023-05-11 19:08:08 -0300

    Agrege dos clases

---
+++ test/ar/edu/unlam/pb2/Pedidos.java
@@ -0,0 +1,27 @@
+package ar.edu.unlam.pb2;
+
+public class Pedidos {
+	private Integer codigo;
+	private String descripcion;
+	private Integer cantidad;
+	public Integer getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(Integer codigo) {
+		this.codigo = codigo;
+	}
+	public String getDescripcion() {
+		return descripcion;
+	}
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+	public Integer getCantidad() {
+		return cantidad;
+	}
+	public void setCantidad(Integer cantidad) {
+		this.cantidad = cantidad;
+	}
+	
+
+}
