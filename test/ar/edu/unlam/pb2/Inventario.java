a18dfa449e230f36159dd913009f9656a7f9025f
Mauricio Galvan <129338837+MauriGalvan@users.noreply.github.com>
2023-05-14 18:10:52 -0300

    Update Inventario.java

--- test/ar/edu/unlam/pb2/Inventario.java
+++ test/ar/edu/unlam/pb2/Inventario.java
@@ -4,33 +4,6 @@ import java.util.ArrayList;
 
 public class Inventario {
 
-	private ArrayList<Producto> productos;
 	
-	
-public Inventario() {
-	
-	this.productos = new ArrayList<>();
-}
-
-public void agregarProducto(Producto producto) {
-	productos.add(producto);
-}
-public Producto buscarProducto (String nombre) {
-	for(Producto producto: productos) {
-		if(producto.getNombre().equals(nombre)) {
-			return producto;
-		}
-	}
-	return null;
-	
-}
-public Producto registrarSalida(String nombre,int cantidad) {
-	Producto producto = buscarProducto(nombre);
-	if(producto!= null && producto.getStock()>=cantidad) {
-		producto.restarStock(cantidad);
-		return producto;
-	}return null;
-	
-}
 
 }
